92dcd9f33dd33d60d1f3321899edec6692ad5ee5
zhangsheng <zhsheng26@gmail.com>
2017-11-14 21:41:51 +0800

    controller

--- src/main/java/cn/springmvc/controller/MainController.java
+++ src/main/java/cn/springmvc/controller/MainController.java
@@ -1,7 +1,9 @@
 package cn.springmvc.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,16 @@ public class MainController {
     @RequestMapping("/greeting/{name}")
     public String greeting(@PathVariable("name") String name) {
         return "hello, " + name;
+    }
+
+    @RequestMapping("/hello")
+    public String hello(@RequestParam(defaultValue = "苟") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+    @RequestMapping("/index")
+    public String index() {
+        return "hello world";
     }
 }
--- src/main/java/cn/springmvc/mvp/MainActivity.java
+++ src/main/java/cn/springmvc/mvp/MainActivity.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainActivity {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
         IBookPresenter bookPresenter = (IBookPresenter) context.getBean("bookPresenter");
         bookPresenter.showBookInfo();
 
