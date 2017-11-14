4798779e9ee78a6e89f95390a338fabf12a1f73c
zhangsheng <zhsheng26@gmail.com>
2017-11-14 10:53:42 +0800

    init spring mvc

---
+++ src/main/java/cn/springmvc/MainController.java
@@ -0,0 +1,13 @@
+package cn.springmvc;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @RequestMapping("/greeting/{name}")
+    public String greeting(@PathVariable("name") String name) {
+        return "hello, " + name;
+    }
+}
