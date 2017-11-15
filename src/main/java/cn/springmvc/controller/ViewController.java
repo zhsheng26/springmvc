cef70e2d133a9acad41333d4163cf96fadd8f088
zhangsheng <zhsheng26@gmail.com>
2017-11-15 09:26:10 +0800

    controller

--- src/main/java/cn/springmvc/controller/ViewController.java
+++ src/main/java/cn/springmvc/controller/ViewController.java
@@ -1,13 +1,13 @@
 package cn.springmvc.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class MainController {
+@Controller
+public class ViewController {
     @RequestMapping("/greeting/{name}")
     public String greeting(@PathVariable("name") String name) {
         return "hello, " + name;
@@ -21,6 +21,6 @@ public class MainController {
 
     @RequestMapping("/index")
     public String index() {
-        return "hello world";
+        return "index";
     }
 }
--- src/main/webapp/WEB-INF/pages/index.jsp
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -6,11 +6,13 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  hello spring
-  </body>
+<head>
+    <title>主页</title>
+</head>
+<body>
+<h1>欢迎</h1>
+<h2><a href="<c:url value="/hello"/>">问候</a></h2>
+</body>
 </html>
